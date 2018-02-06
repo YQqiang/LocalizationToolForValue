499bb403b197d2b3cd787d41e6bd745365be3e56
yuqiang <1054572107@qq.com>
2018-02-06 14:33:51 +0800

    Initial Commit

---
+++ LocalizationToolForValue/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LocalizationToolForValue
+//
+//  Created by sungrow on 2018/2/6.
+//  Copyright © 2018年 sungrow. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
